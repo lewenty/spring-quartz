d7a176f6f2b890665137a74edcfd19af69298bee
levent.yildiz <levent.yildiz@turkkep.com.tr>
2014-10-12 23:10:56 +0300

    reformat code..

--- src/main/java/com/company/service/impl/MailReportServiceImpl.java
+++ src/main/java/com/company/service/impl/MailReportServiceImpl.java
@@ -2,12 +2,23 @@ package com.company.service.impl;
 
 import com.company.service.MailReportService;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * Created by levent.yildiz on 12.10.2014.
  */
 public class MailReportServiceImpl implements MailReportService {
     @Override
     public void mailSend() {
-        System.out.println("Gonderiliyor..");
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Calendar calendar = Calendar.getInstance();
+        String strDate = dateFormat.format(calendar.getTime());
+
+        //TODO : development
+        //Mail gönderme işlemi burada yapılacak
+        System.out.println("Mail Gonderiliyor.." + strDate);
     }
 }
